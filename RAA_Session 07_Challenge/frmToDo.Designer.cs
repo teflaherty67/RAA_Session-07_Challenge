0457d7cdfdc17feac681d2165c9a762516a8587c
Tom E Flaherty <teflaherty@utexas.edu>
2023-01-29 12:49:09 -0600

    Add project files.

---
+++ RAA_Session 07_Challenge/frmToDo.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace RAA_Session_07_Challenge
+{
+    partial class frmToDo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbxToDo = new System.Windows.Forms.ListBox();
+            this.tbxAddEdit = new System.Windows.Forms.TextBox();
+            this.btnAddEdit = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To Do Items";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(506, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Add New Item";
+            // 
+            // lbxToDo
+            // 
+            this.lbxToDo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbxToDo.FormattingEnabled = true;
+            this.lbxToDo.ItemHeight = 31;
+            this.lbxToDo.Location = new System.Drawing.Point(27, 70);
+            this.lbxToDo.Name = "lbxToDo";
+            this.lbxToDo.Size = new System.Drawing.Size(446, 686);
+            this.lbxToDo.TabIndex = 2;
+            // 
+            // tbxAddEdit
+            // 
+            this.tbxAddEdit.Location = new System.Drawing.Point(512, 70);
+            this.tbxAddEdit.Name = "tbxAddEdit";
+            this.tbxAddEdit.Size = new System.Drawing.Size(434, 38);
+            this.tbxAddEdit.TabIndex = 3;
+            // 
+            // btnAddEdit
+            // 
+            this.btnAddEdit.Location = new System.Drawing.Point(512, 123);
+            this.btnAddEdit.Name = "btnAddEdit";
+            this.btnAddEdit.Size = new System.Drawing.Size(160, 40);
+            this.btnAddEdit.TabIndex = 4;
+            this.btnAddEdit.Text = "Add Item";
+            this.btnAddEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(512, 316);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(125, 40);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(512, 376);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(125, 40);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(512, 508);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(100, 40);
+            this.btnUp.TabIndex = 7;
+            this.btnUp.Text = "Up";
+            this.btnUp.UseVisualStyleBackColor = true;
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(512, 569);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(100, 40);
+            this.btnDown.TabIndex = 8;
+            this.btnDown.Text = "Dn";
+            this.btnDown.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(506, 724);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 32);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Add New Item";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(506, 683);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 32);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Add New Item";
+            // 
+            // frmToDo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 801);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAddEdit);
+            this.Controls.Add(this.tbxAddEdit);
+            this.Controls.Add(this.lbxToDo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmToDo";
+            this.Text = "frmToDo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbxToDo;
+        private System.Windows.Forms.TextBox tbxAddEdit;
+        private System.Windows.Forms.Button btnAddEdit;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
